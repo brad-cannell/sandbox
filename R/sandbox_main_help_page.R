b9cc3918946e8b9d1586f841fcec8e215f027019
Brad Cannell <brad.cannell@gmail.com>
2016-03-28 10:41:04 -0500

    Initial commit

---
+++ R/sandbox_main_help_page.R
@@ -0,0 +1,12 @@
+#' Test Stuff Out
+#'
+#' This is just a sandbox for me to test out package stuff and Github.
+#'
+#' @section Notes:
+#' This help page should be in synce with DESCRIPTION. Easy for users to find
+#' using \code{?<package-name>}. Should describe the most important components of the
+#' package.
+#'
+#' @docType package
+#' @name sandbox
+NULL
